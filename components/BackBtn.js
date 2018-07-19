53b75296e6abe31249fb7f004dfe4f7e01dbdde9
CHBaker <charlesprobaker@mail.com>
2018-07-19 15:55:03 -0600

    FEATURE: add form to add new deck
    - add backbtn component

---
+++ components/BackBtn.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+export function BackBtn ({ onPress }) {
+    return (
+        <TouchableOpacity onPress={ onPress } style={ styles.backHeader }>
+            <FontAwesome name='arrow-left' size={ 30 } color='white' />
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    backHeader: {
+        alignSelf: 'stretch',
+        paddingLeft: 10,
+        height: 40,
+        backgroundColor: 'red',
+    }
+})
--- components/NewDeck.js
+++ components/NewDeck.js
@@ -3,6 +3,8 @@ import {
     View, TouchableOpacity, Text, Platform,
     StyleSheet, TextInput
 } from 'react-native'
+import { connect } from 'react-redux'
+import { BackBtn } from './BackBtn'
 
 class NewDeck extends Component {
 
@@ -12,31 +14,44 @@ class NewDeck extends Component {
 
     handleSubmit () {
         console.log(this.state)
+
+        this.setState({ title: null })
+    }
+
+    goBack() {
+        console.log('back')
     }
 
     render () {
         return (
-            <View>
-                <Text>
-                    What is the name of your new deck?
-                </Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeTitle={(title) => this.setState({title})}
-                    value={this.state.title}
-                />
-                <TouchableOpacity
-                    style={ styles.submit }
-                    onPress={ () => this.handleSubmit() }
-                >
-                    <Text>Submit</Text>
-                </TouchableOpacity>
+            <View style={{ flex: 1 }}>
+                <BackBtn onPress={ this.goBack() }/>
+                <View style={ styles.container }>
+                    <Text style={ styles.header }>
+                        What is the name
+                    </Text>
+                    <Text style={ styles.header }>
+                        of your new deck?
+                    </Text>
+                        <TextInput
+                            style={ styles.input }
+                            multiline={ true }
+                            onChangeText={(title) => this.setState({title})}
+                            value={this.state.title}
+                        />
+                    <TouchableOpacity
+                        style={ styles.submit }
+                        onPress={ this.handleSubmit.bind(this) }
+                    >
+                        <Text style={ styles.submitText }>Submit</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
 
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -44,24 +59,42 @@ styles = StyleSheet.create({
         flexDirection: 'column'
     },
     header: {
-        fontSize: 24,
+        fontSize: 38,
         color: 'black',
-        alignSelf: 'stretch'
+        textAlign: 'center'
     },
     input: {
         borderWidth: 2,
-        borderColor: 'red',
+        borderColor: 'black',
+        backgroundColor: 'white',
         borderRadius: 7,
-        marginTop: 10,
-        marginBottom: 10,
-        padding: 5
+        marginTop: 50,
+        marginBottom: 50,
+        height: 50,
+        fontSize: 25,
+        marginLeft: 40,
+        marginRight: 40,
+        alignSelf: 'stretch',
+        textAlign: 'center',
+        paddingTop: 8
     },
     submit: {
-        fontSize: 20,
         padding: 5,
+        height: 53,
         backgroundColor: 'red',
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 60,
+        marginLeft: 60,
+        borderRadius: 7
+    },
+    submitText: {
         color: 'white',
+        fontSize: 28,
     }
 })
 
-export default NewDeck;
+
+
+export default connect()(NewDeck)
